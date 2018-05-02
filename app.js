5e986111084f048f2030a7f59b5e30112f9a18c3
Luisa9 <38859893+Luisa9@users.noreply.github.com>
2018-05-02 12:57:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -49,12 +49,12 @@ mongoose.connect('mongodb://ed:ed@ds237489.mlab.com:37489/heroku_4jqslj1n');
 var db = mongoose.connection;
 
 // show register form
-app.get("/register", function(req, res){
+app.get("https://luisa9.github.io/ptsi/register", function(req, res){
      req.flash("reg", "register here");
    res.render("register", {page: 'register'}); 
 });
 
-app.get("/site", function(req, res){
+app.get("https://luisa9.github.io/ptsi/site", function(req, res){
    res.render("site", {page: 'site'}); 
 });
 
@@ -65,7 +65,7 @@ app.get("/secret",isLoggedIn, function(req, res){
 
 
 //handling user sign up
-app.post("/register", function(req, res){
+app.post("https://luisa9.github.io/ptsi/register", function(req, res){
     var newUser = new User({
         username: req.body.username,
       
@@ -83,20 +83,20 @@ app.post("/register", function(req, res){
             return res.render("register", {error: err.message});
         }
         passport.authenticate("local")(req, res, function(){
-           res.redirect("/login"); 
+           res.redirect("https://luisa9.github.io/ptsi/site"); 
         });
     });
 });
 
-app.get("/login", function(req, res){
+app.get("https://luisa9.github.io/ptsi/login", function(req, res){
    res.render("login", {page: 'login'}); 
 });
 
 
-app.post("/login", passport.authenticate("local", 
+app.post("https://luisa9.github.io/ptsi/login", passport.authenticate("local", 
     {
-        successRedirect: "/site",
-        failureRedirect: "/login",
+        successRedirect: "https://luisa9.github.io/ptsi/site",
+        failureRedirect: "https://luisa9.github.io/ptsi/login",
         failureFlash: true,
         successFlash: 'Welcome'
     }), function(req, res){
