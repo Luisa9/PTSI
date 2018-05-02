80707d2fd23e62b7fd41dddf7d75f03e2c728eba
Luisa9 <38859893+Luisa9@users.noreply.github.com>
2018-05-02 14:41:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ var session = require("express-session");
 var client = require("twilio");
 var http = require('http');
 
+http.createServer(function(req, res) {  
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  res.write("https://luisa9.github.io/ptsi/login");
+  res.end();
+  
 app.configure(function() {
   app.use(express.cookieParser('keyboard cat'));
   app.use(express.session({ cookie: { maxAge: 60000 }}));
