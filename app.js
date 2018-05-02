2540350c85e14c2ddce778230193fe03a943defb
Luisa9 <38859893+Luisa9@users.noreply.github.com>
2018-05-02 12:15:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -83,7 +83,6 @@ app.post("/register", function(req, res){
             return res.render("register", {error: err.message});
         }
         passport.authenticate("local")(req, res, function(){
-           req.flash("success", "Successfully Signed Up! Nice to meet you " + req.body.username);
            res.redirect("/site"); 
         });
     });
